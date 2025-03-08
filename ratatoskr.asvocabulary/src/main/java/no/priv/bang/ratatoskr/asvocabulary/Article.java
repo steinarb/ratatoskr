3dffb7f6ed3e628c5e387ffba94ed7c1aa3cef0a
Steinar Bang <sb@dod.no>
2025-03-08 17:34:07 +0100

    Add builder with copy to AS Article

--- ratatoskr.asvocabulary/src/main/java/no/priv/bang/ratatoskr/asvocabulary/Article.java
+++ ratatoskr.asvocabulary/src/main/java/no/priv/bang/ratatoskr/asvocabulary/Article.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Steinar Bang
+ * Copyright 2024-2025 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,4 +52,263 @@ public record Article(
     Collection replies,
     LinkOrObject tag
 ) implements ActivityStreamObject {
+
+    public static Builder with() {
+        return new Builder();
+    }
+
+    public static Builder with(Article article) {
+        var builder = new Builder();
+        builder.context = article.context();
+        builder.id = article.id();
+        builder.name = article.name();
+        builder.nameMap = article.nameMap();
+        builder.summary = article.summary();
+        builder.summaryMap = article.summaryMap();
+        builder.content = article.content();
+        builder.contentMap = article.contentMap();
+        builder.mediaType = article.mediaType();
+        builder.url = article.url();
+        builder.attributedTo = article.attributedTo();
+        builder.duration = article.duration();
+        builder.startTime = article.startTime();
+        builder.endTime = article.endTime();
+        builder.published = article.published();
+        builder.updated = article.updated();
+        builder.attachment = article.attachment();
+        builder.audience = article.audience();
+        builder.to = article.to();
+        builder.bcc = article.bcc();
+        builder.bto = article.bto();
+        builder.cc = article.cc();
+        builder.generator = article.generator();
+        builder.icon = article.icon();
+        builder.image = article.image();
+        builder.inReplyTo = article.inReplyTo();
+        builder.location = article.location();
+        builder.preview = article.preview();
+        builder.replies = article.replies();
+        builder.tag = article.tag();
+        return builder;
+    }
+
+    public static class Builder {
+
+        private Object context;
+        private String id;
+        private String name;
+        private Map<String, String> nameMap;
+        private String summary;
+        private Map<String, String> summaryMap;
+        private String content;
+        private Map<String, String> contentMap;
+        private String mediaType;
+        private List<Link> url;
+        private LinkOrObject attributedTo;
+        private String duration;
+        private ZonedDateTime startTime;
+        private ZonedDateTime endTime;
+        private ZonedDateTime published;
+        private ZonedDateTime updated;
+        private LinkOrObject attachment;
+        private LinkOrObject audience;
+        private LinkOrObject to;
+        private LinkOrObject bcc;
+        private LinkOrObject bto;
+        private LinkOrObject cc;
+        private LinkOrObject generator;
+        private LinkOrObject icon;
+        private LinkOrObject image;
+        private LinkOrObject inReplyTo;
+        private LinkOrObject location;
+        private LinkOrObject preview;
+        private Collection replies;
+        private LinkOrObject tag;
+
+        public Article build() {
+            return new Article(
+                context,
+                ActivityStreamObjectType.Article,
+                id,
+                name,
+                nameMap,
+                summary,
+                summaryMap,
+                content,
+                contentMap,
+                mediaType,
+                url,
+                attributedTo,
+                duration,
+                startTime,
+                endTime,
+                published,
+                updated,
+                attachment,
+                audience,
+                to,
+                bcc,
+                bto,
+                cc,
+                generator,
+                icon,
+                image,
+                inReplyTo,
+                location,
+                preview,
+                replies,
+                tag
+            );
+        }
+
+        public Builder context(Object context) {
+            this.context = context;
+            return this;
+        }
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder nameMap(Map<String, String> nameMap) {
+            this.nameMap = nameMap;
+            return this;
+        }
+
+        public Builder summary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public Builder summaryMap(Map<String, String> summaryMap) {
+            this.summaryMap = summaryMap;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder contentMap(Map<String, String> contentMap) {
+            this.contentMap = contentMap;
+            return this;
+        }
+
+        public Builder mediaType(String mediaType) {
+            this.mediaType = mediaType;
+            return this;
+        }
+
+        public Builder url(List<Link> url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder attributedTo(LinkOrObject attributedTo) {
+            this.attributedTo = attributedTo;
+            return this;
+        }
+
+        public Builder duration(String duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public Builder startTime(ZonedDateTime startTime) {
+            this.startTime = startTime;
+            return this;
+        }
+
+        public Builder endTime(ZonedDateTime endTime) {
+            this.endTime = endTime;
+            return this;
+        }
+
+        public Builder published(ZonedDateTime published) {
+            this.published = published;
+            return this;
+        }
+
+        public Builder updated(ZonedDateTime updated) {
+            this.updated = updated;
+            return this;
+        }
+
+        public Builder attachment(LinkOrObject attachment) {
+            this.attachment = attachment;
+            return this;
+        }
+
+        public Builder audience(LinkOrObject audience) {
+            this.audience = audience;
+            return this;
+        }
+
+        public Builder to(LinkOrObject to) {
+            this.to = to;
+            return this;
+        }
+
+        public Builder bcc(LinkOrObject bcc) {
+            this.bcc = bcc;
+            return this;
+        }
+
+        public Builder bto(LinkOrObject bto) {
+            this.bto = bto;
+            return this;
+        }
+
+        public Builder cc(LinkOrObject cc) {
+            this.cc = cc;
+            return this;
+        }
+
+        public Builder generator(LinkOrObject generator) {
+            this.generator = generator;
+            return this;
+        }
+
+        public Builder icon(LinkOrObject icon) {
+            this.icon = icon;
+            return this;
+        }
+
+        public Builder image(LinkOrObject image) {
+            this.image = image;
+            return this;
+        }
+
+        public Builder inReplyTo(LinkOrObject inReplyTo) {
+            this.inReplyTo = inReplyTo;
+            return this;
+        }
+
+        public Builder location(LinkOrObject location) {
+            this.location = location;
+            return this;
+        }
+
+        public Builder preview(LinkOrObject preview) {
+            this.preview = preview;
+            return this;
+        }
+
+        public Builder replies(Collection replies) {
+            this.replies = replies;
+            return this;
+        }
+
+        public Builder tag(LinkOrObject tag) {
+            this.tag = tag;
+            return this;
+        }
+    }
 }
---
+++ ratatoskr.asvocabulary/src/test/java/no/priv/bang/ratatoskr/asvocabulary/ArticleTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2025 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.ratatoskr.asvocabulary;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class ArticleTest {
+
+    @Test
+    void testCreate() {
+        Object context = null;
+        String id = "https://sally.example.com/posts/1";
+        String name = "What a Crazy Day I Had";
+        Map<String, String> nameMap = Collections.emptyMap();
+        String summary = "Followers collection";
+        Map<String, String> summaryMap = Collections.emptyMap();
+        String content = "<div>... you will never believe ...</div>";
+        Map<String, String> contentMap = Collections.emptyMap();
+        String mediaType = "";
+        List<Link> url = Collections.emptyList();
+        LinkOrObject attributedTo = Link.with().href("https://sally.example.com").build();
+        String duration = "";
+        ZonedDateTime startTime = null;
+        ZonedDateTime endTime = null;
+        ZonedDateTime published = null;
+        ZonedDateTime updated = null;
+        LinkOrObject attachment = null;
+        LinkOrObject audience = null;
+        LinkOrObject to = null;
+        LinkOrObject bcc = null;
+        LinkOrObject bto = null;
+        LinkOrObject cc = null;
+        LinkOrObject generator = null;
+        LinkOrObject icon = null;
+        LinkOrObject image = null;
+        LinkOrObject inReplyTo = null;
+        LinkOrObject location = null;
+        LinkOrObject preview = null;
+        Collection replies = null;
+        LinkOrObject tag = null;
+
+        var article = Article.with()
+            .context(context)
+            .id(id)
+            .name(name)
+            .nameMap(nameMap)
+            .summary(summary)
+            .summaryMap(summaryMap)
+            .content(content)
+            .contentMap(contentMap)
+            .mediaType(mediaType)
+            .url(url)
+            .attributedTo(attributedTo)
+            .duration(duration)
+            .startTime(startTime)
+            .endTime(endTime)
+            .published(published)
+            .updated(updated)
+            .attachment(attachment)
+            .audience(audience)
+            .to(to)
+            .bcc(bcc)
+            .bto(bto)
+            .cc(cc)
+            .generator(generator)
+            .icon(icon)
+            .image(image)
+            .inReplyTo(inReplyTo)
+            .location(location)
+            .preview(preview)
+            .replies(replies)
+            .tag(tag)
+            .build();
+
+        assertThat(article.type()).isEqualTo(ActivityStreamObjectType.Article);
+        assertThat(article.name()).isEqualTo(name);
+        assertThat(article.content()).isEqualTo(content);
+        assertThat(article.attributedTo()).isEqualTo(attributedTo);
+    }
+
+    @Test
+    void testCopyAndModify() {
+        Object context = null;
+        String id = "https://sally.example.com/posts/1";
+        String name = "What a Crazy Day I Had";
+        Map<String, String> nameMap = Collections.emptyMap();
+        String summary = "Followers collection";
+        Map<String, String> summaryMap = Collections.emptyMap();
+        String content = "<div>... you will never believe ...</div>";
+        Map<String, String> contentMap = Collections.emptyMap();
+        String mediaType = "";
+        List<Link> url = Collections.emptyList();
+        String duration = "";
+        ZonedDateTime startTime = null;
+        ZonedDateTime endTime = null;
+        ZonedDateTime published = null;
+        ZonedDateTime updated = null;
+        LinkOrObject attachment = null;
+        LinkOrObject audience = null;
+        LinkOrObject to = null;
+        LinkOrObject bcc = null;
+        LinkOrObject bto = null;
+        LinkOrObject cc = null;
+        LinkOrObject generator = null;
+        LinkOrObject icon = null;
+        LinkOrObject image = null;
+        LinkOrObject inReplyTo = null;
+        LinkOrObject location = null;
+        LinkOrObject preview = null;
+        Collection replies = null;
+        LinkOrObject tag = null;
+
+        var article = Article.with()
+            .context(context)
+            .id(id)
+            .name(name)
+            .nameMap(nameMap)
+            .summary(summary)
+            .summaryMap(summaryMap)
+            .content(content)
+            .contentMap(contentMap)
+            .mediaType(mediaType)
+            .url(url)
+            .attributedTo(null)
+            .duration(duration)
+            .startTime(startTime)
+            .endTime(endTime)
+            .published(published)
+            .updated(updated)
+            .attachment(attachment)
+            .audience(audience)
+            .to(to)
+            .bcc(bcc)
+            .bto(bto)
+            .cc(cc)
+            .generator(generator)
+            .icon(icon)
+            .image(image)
+            .inReplyTo(inReplyTo)
+            .location(location)
+            .preview(preview)
+            .replies(replies)
+            .tag(tag)
+            .build();
+
+        var attributedTo = Link.with().href("https://sally.example.com").build();
+        var articleCopy = Article.with(article).attributedTo(attributedTo).build();
+
+        assertThat(articleCopy.type()).isEqualTo(ActivityStreamObjectType.Article);
+        assertThat(articleCopy.name()).isEqualTo(name);
+        assertThat(articleCopy.content()).isEqualTo(content);
+        assertThat(articleCopy.attributedTo()).isEqualTo(attributedTo);
+    }
+
+}
