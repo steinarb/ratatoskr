b53f7b21027e3e892423b20eaa15842475be806e
Steinar Bang <sb@dod.no>
2026-05-14 13:03:25 +0200

    Add boolean field "sensitive" to "Note" in no.priv.bang.ratatoskr.asvocabulary to match example used in test parsing mastodon post

--- ratatoskr.asvocabulary/src/main/java/no/priv/bang/ratatoskr/asvocabulary/Note.java
+++ ratatoskr.asvocabulary/src/main/java/no/priv/bang/ratatoskr/asvocabulary/Note.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Steinar Bang
+ * Copyright 2024-2026 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,7 +50,8 @@ public record Note(
     LinkOrObject location,
     LinkOrObject preview,
     Collection replies,
-    LinkOrObject tag
+    LinkOrObject tag,
+    boolean sensitive
 ) implements ActivityStreamObject
 {
 }
